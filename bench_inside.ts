608d0ffc37211f65f5233f64544f8744e3e1a033
Cyan Changes <contact@cyans.me>
2025-02-16 17:47:30 +0800

    feat: fancy boxes

--- bench_inside.ts
+++ bench_inside.ts
@@ -7,34 +7,92 @@ import {
   mitata,
   reset_collections,
 } from "./mitata_fmt.ts";
+import process from "node:process";
 
 const ROOT = import.meta.dir + "/";
 const TESTS = ROOT + "tests/";
 const DIST = ROOT + "dist/";
+let bannerLayer = 0;
 
 if (fs.existsSync(DIST)) fs.rmSync(DIST, { recursive: true });
 
+function log(content: string, sym: string = "*") {
+  const x = `${$.green}${sym}${$.cyan} ${content}`;
+  if (bannerLayer) {
+    bannerContent(x);
+  } else console.log(x);
+}
+
+function bannerHeader(header: string) {
+  bannerLayer++;
+  const def = (process.stdout.columns ?? 20) - 2 - 9 - Bun.stringWidth(header);
+  const mid = Math.floor(
+    def / 2,
+  );
+  console.log(
+    $.yellow +
+      $.boxplot.symbols.tl +
+      $.boxplot.symbols.h.repeat(mid - 2) +
+      "[ " + $.reset + header + $.yellow + " ]" +
+      $.yellow +
+      $.boxplot.symbols.h.repeat(def - mid - 2) +
+      $.boxplot.symbols.tr,
+  );
+}
+
+function bannerContent(content: string) {
+  const border = $.yellow + $.boxplot.symbols.v + $.reset;
+
+  const lines = content.split("\n")
+    .map((line) =>
+      (border + line).padEnd((process.stdout.columns ?? 20) - 1) + border
+    )
+    .join("\n");
+
+  console.log(lines);
+}
+
+function bannerFooter() {
+  bannerLayer--;
+  console.log(
+    $.yellow +
+      $.boxplot.symbols.bl +
+      $.boxplot.symbols.h.repeat((process.stdout.columns ?? 20) - 2) +
+      $.boxplot.symbols.br,
+  );
+}
+
+log("Bundling files...");
+
 await Bun.build({
-  entrypoints: [
-    ...new Bun.Glob("**/*.js").scanSync({
+  entrypoints: await Array.fromAsync(
+    new Bun.Glob("**/*.js").scan({
       cwd: TESTS,
       absolute: true,
     }),
-  ],
+  ),
   outdir: DIST,
   target: "bun",
 });
 
-const opts = {};
+const opts = {
+  print(x) {
+    bannerContent(x);
+  },
+};
 defaults(opts);
 
-for (const test of new Bun.Glob("**/*.js").scanSync(DIST)) {
-  console.log("*", test.substring(0, test.lastIndexOf(".")));
+log("Starting Benchmark...");
+
+for await (const test of new Bun.Glob("**/*.js").scan(DIST)) {
+  bannerHeader();
+
+  log(test.substring(0, test.lastIndexOf(".")), "@");
 
   let ctx;
   let benchmarks = [];
   for (const runtime in commands) {
-    console.log("+", runtime);
+    log(runtime, "+");
     const { context: ctx_, benchmarks: bm } = await Bun.$`${{
       raw: commands[runtime],
     }} ${DIST + test}`
@@ -79,4 +137,6 @@ for (const test of new Bun.Glob("**/*.js").scanSync(DIST)) {
   await mitata(ctx, opts, benchmarks);
 
   reset_collections();
+
+  bannerFooter();
 }
